85b59892e3274164fe5aed263eab163cedfdcbcf
Faris Chebib <faris@zaneprep.com>
2013-12-01 19:12:10 -0700

    Added login view logic

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -3,5 +3,5 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('profiles.views.home',
-    url(r'^$', 'profile_home', name='home'),
+    url(r'^$', 'index', name='home'),
 )
--- apps/profiles/views/home.py
+++ apps/profiles/views/home.py
@@ -1,11 +1,30 @@
 # encoding: utf-8
 
-from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, get_object_or_404, redirect
 from profiles.forms import LoginForm
+from profiles.models import UttrUser
 
 def login(request):
     ctx = {}
-    ctx['login_form'] = LoginForm()
+    login_form = LoginForm()
+    ctx['login_form'] = login_form
+
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        ctx['login_form'] = login_form
+        if login_form.is_valid():
+            email = login_form.cleaned_data['email']
+            password = login_form.cleaned_data['password']
+            try:
+                user = UttrUser.objects.get(email=email)
+            except UttrUser.DoesNotExist:
+                return render(request, 'profiles/login.html', ctx)
+
+            if not user.check_password(password):
+                return render(request, 'profiles/login.html', ctx)
+
+            return redirect(reverse('profiles:home'))
 
     return render(request, 'profiles/login.html', ctx)
 
