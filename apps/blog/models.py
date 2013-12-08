e660e1f2acc133dbd61ecef0380defdf572346eb
Faris Chebib <faris@zaneprep.com>
2013-12-08 12:58:25 -0700

    added initial model outlines

---
+++ apps/blog/models.py
@@ -0,0 +1,7 @@
+# encoding: utf-8
+# models.py
+
+from django.db import models
+
+class BlogPost(models.Model):
+	pass
---
+++ apps/polls/models.py
@@ -0,0 +1,11 @@
+# encoding: utf-8
+# models.py
+
+
+from django.db import models
+
+class PollQuestion(models.Model):
+	pass
+
+class Poll(models.Model):
+	pass
