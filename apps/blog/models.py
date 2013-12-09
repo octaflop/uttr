4f0c59c5e64a61fa1c08500a9978a8096bcd7f54
Faris Chebib <faris@zaneprep.com>
2013-12-08 17:10:20 -0700

    added blog

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -4,5 +4,12 @@
 from django.db import models
 from uttr.models.mixins import TimestampMixin
 
-class BlogPost(models.Model):
-    pass
+class BlogPost(TimestampMixin):
+    title = models.CharField(max_length=250)
+    author = models.ForeignKey('profiles.UttrUser')
+    entry = models.TextField()
+
+
+class Source(models.Model):
+    url = models.URLField(blank=True)
+    author = models.CharField(max_length=250, blank=True)
