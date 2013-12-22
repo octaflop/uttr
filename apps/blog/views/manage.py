e95fcf0014cb5b020d62d4f465faea01c1fba303
Faris Chebib <faris@zaneprep.com>
2013-12-21 22:09:26 -0700

    Added usernav bar and looping modelform

--- apps/blog/views/manage.py
+++ apps/blog/views/manage.py
@@ -1,6 +1,9 @@
 #encoding: utf-8
 
 from django.shortcuts import render, render_to_response
+from django.template import RequestContext
+
+from blog.forms import BlogForm
 
 def create(request):
     """
@@ -9,4 +12,7 @@ def create(request):
     ctx = {}
     template_name = 'blog/manage/create.html'
 
-    return render_to_response(template_name, ctx)
+    blog_form = BlogForm()
+    ctx['blog_form'] = blog_form
+
+    return render_to_response(template_name, RequestContext(request, ctx))
