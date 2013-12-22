633c9fd084f07d80aa75a293e1287b37729460a0
Faris Chebib <faris@zaneprep.com>
2013-12-21 22:56:34 -0700

    added custom stylings

--- apps/blog/views/manage.py
+++ apps/blog/views/manage.py
@@ -1,9 +1,15 @@
 #encoding: utf-8
 
+import datetime
+
+from profiles.models import UttrUser
+
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
+from django.contrib import messages
 
 from blog.forms import BlogForm
+from blog.models import BlogPost
 
 def create(request):
     """
@@ -13,7 +19,23 @@ def create(request):
     ctx['compose_active'] = True
     template_name = 'blog/manage/create.html'
 
-    blog_form = BlogForm()
+    blog_form = BlogForm(initial={'author': request.user})
     ctx['blog_form'] = blog_form
+
+    if request.method == 'POST':
+        blog_form = BlogForm(request.POST, initial={'author': request.user})
+        ctx['blog_form'] = blog_form
+
+        if blog_form.is_valid():
+            blog = blog_form.save(commit=False)
+            blog.author = request.user
+            blog.save()
+            message = "Your post was saved successfully!"
+            messages.success(request, message)
+
+        else:
+            message = "Sorry, there was an issue with your post"
+            messages.error(request, message)
+
 
     return render_to_response(template_name, RequestContext(request, ctx))
