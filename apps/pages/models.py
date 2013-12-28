93fd4ba8ad58f3fb3ea32ce249f7f803170c8bee
Faris Chebib <faris@zaneprep.com>
2013-12-28 14:07:16 -0700

    altered upload to

--- apps/pages/models.py
+++ apps/pages/models.py
@@ -14,7 +14,7 @@ class File(models.Model):
     """
     A basic file-upload field
     """
-    uttr_file = models.FileField(upload_to='static/pages/files')
+    uttr_file = models.FileField(upload_to='pages/files')
 
     def __unicode__(self):
         return self.uttr_file.file.name
