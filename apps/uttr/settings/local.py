85a1a172d89f46fb690f614a1c7b438217695940
Faris Chebib <faris@zaneprep.com>
2013-12-30 21:07:46 -0700

    Added mandrill to app

--- apps/uttr/settings/local.py
+++ apps/uttr/settings/local.py
@@ -3,6 +3,10 @@ from .base import *
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = '/tmp/uttr-messages'
+DEFAULT_FROM_EMAIL = 'digitaltextbookstudy@zaneprep.com'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -11,3 +15,4 @@ DATABASES = {
 }
 
 WSGI_APPLICATION = 'uttr.wsgi.dev.application'
+
