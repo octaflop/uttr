8c508fd06e507d755cbb43d124ccc264975ffe6c
Faris Chebib <faris@zaneprep.com>
2014-01-22 00:19:07 -0700

    added admin, migrations, and CKEditor refs

---
+++ apps/forums/admin.py
@@ -0,0 +1,26 @@
+# encoding: utf-8
+
+from django import forms
+from django.contrib import admin
+
+from ckeditor.widgets import CKEditorWidget
+
+from forums.models import Topic, Reply
+
+class TopicAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorWidget())
+    class Meta:
+        model = Topic
+
+
+class TopicAdmin(admin.ModelAdmin):
+    form = TopicAdminForm
+
+
+class ReplyAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Reply
+
+
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Reply, ReplyAdmin)
