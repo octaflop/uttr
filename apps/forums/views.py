7d5744ef5b19c81188ab22f443a3820aeed8422c
Faris Chebib <faris@zaneprep.com>
2014-01-23 21:41:17 -0700

    added base36 refs

--- apps/forums/views.py
+++ apps/forums/views.py
@@ -1,6 +1,8 @@
 # encoding: utf-8
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils.http import base36_to_int, int_to_base36
+from django.core.urlresolvers import reverse
 
 from forums.models import Topic, Reply
 
@@ -13,16 +15,23 @@ def topic_list(request):
 
     return render(request, template_name, ctx)
 
-def topic_view(request, slug):
+def topic_view_by_slug(request, slug):
+    """
+    redirects to bid-based view
+    """
+    topic = Topic.objects.get(slug=slug)
+    return redirect(reverse('forums:view', kwargs={'bid': int_to_base36(topic.id)}))
+
+def topic_view(request, bid):
     """
     List the topic at the top and disply replies
     """
     ctx = {}
     template_name = 'forums/topic_view.html'
 
-    topic = Topic.objects.get(slug=slug)
+    topic = Topic.objects.get(id=base36_to_int(bid))
     ctx['topic'] = topic
 
-    ctx['replies'] = Reply.objects.filter(topic__slug=slug, status='posted')
+    ctx['replies'] = Reply.objects.filter(topic=topic, status='posted')
 
     return render(request, template_name, ctx)
