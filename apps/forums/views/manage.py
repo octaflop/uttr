33c932dfeb14aad1b677223360b63cbb9db1120e
Faris Chebib <faris@zaneprep.com>
2014-02-02 16:58:37 -0700

    adding messaging

--- apps/forums/views/manage.py
+++ apps/forums/views/manage.py
@@ -1,5 +1,7 @@
 # encoding: utf-8
 
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.utils.http import base36_to_int, int_to_base36
 from django.core.urlresolvers import reverse
@@ -7,6 +9,8 @@ from django.core.urlresolvers import reverse
 from forums.models import Topic, Reply
 from forums.forms import TopicForm
 
+
+@staff_member_required
 def topic_create(request):
     ctx = {}
     template_name = 'forums/topic_create.html'
@@ -16,7 +20,9 @@ def topic_create(request):
     if request.method == "POST":
         topic_form = TopicForm(request.POST)
         if topic_form.is_valid():
-            topic_form.save()
+            topic = topic_form.save()
+            messages.success(request, "Topic <strong>%s</strong> was created" % (topic.title))
+            return redirect(topic.get_absolute_url())
 
     ctx['topic_form'] = topic_form
     return render(request, template_name, ctx)
@@ -28,6 +34,7 @@ def topic_view_by_slug(request, slug):
     topic = Topic.objects.get(slug=slug)
     return redirect(reverse('forums:view', kwargs={'bid': int_to_base36(topic.id)}))
 
+@staff_member_required
 def topic_edit(request, bid):
     """
     Edit the topic
